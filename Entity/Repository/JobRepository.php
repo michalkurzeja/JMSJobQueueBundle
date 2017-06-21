ce45087b3e358764da1954a5f742960ba204d28d
Michał Kurzeja <mike.kurzeja@gmail.com>
2017-06-21 11:03:35 +0200

    Emergency hack: removed dependant jobs lookup

--- Entity/Repository/JobRepository.php
+++ Entity/Repository/JobRepository.php
@@ -330,16 +330,18 @@ class JobRepository extends EntityRepository
 
     public function findIncomingDependencies(Job $job)
     {
-        return $this->_em->createQuery("SELECT j FROM JMSJobQueueBundle:Job j LEFT JOIN j.dependencies d WHERE :job MEMBER OF j.dependencies")
-                    ->setParameter('job', $job)
-                    ->getResult();
+        return []; // Temporary fix to remove the query (we don't need it, it's EMERGENCY!)
+        //$this->_em->createQuery("SELECT j FROM JMSJobQueueBundle:Job j LEFT JOIN j.dependencies d WHERE :job MEMBER OF j.dependencies")
+        //            ->setParameter('job', $job)
+        //            ->getResult();
     }
 
     public function getIncomingDependencies(Job $job)
     {
-        return $this->_em->createQuery("SELECT j FROM JMSJobQueueBundle:Job j WHERE :job MEMBER OF j.dependencies")
-                    ->setParameter('job', $job)
-                    ->getResult();
+        return []; // Temporary fix to remove the query (we don't need it, it's EMERGENCY!)
+        //$this->_em->createQuery("SELECT j FROM JMSJobQueueBundle:Job j WHERE :job MEMBER OF j.dependencies")
+        //            ->setParameter('job', $job)
+        //            ->getResult();
     }
 
     public function findLastJobsWithError($nbJobs = 10)
